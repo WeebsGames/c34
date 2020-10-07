b4863c683e168a452fff6f581d9a7712235b9a8d
WeebsGames <68392559+WeebsGames@users.noreply.github.com>
2020-10-06 21:18:26 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+//Create variables here
+var dog, happyDog, database, foods, foodStock, dogimg1, dogimg2;
+function preload()
+{
+  //load images here
+  dogimg1 = loadImage("dogImg.png");
+  dogimg2 = loadImage("dogImg1.png");
+}
+
+function setup() {
+	createCanvas(500, 500);
+  dog = createSprite(250,250,50,50)
+  dog.addAnimation("dog",dogimg1);
+  dog.scale=0.1;
+
+  foodStock=database.ref('food');
+  foodStock.on("value", readStock);
+}
+
+
+function draw() {  
+  background(0,175,0);
+  drawSprites();
+  text("food: " + readStock);
+  //add styles here
+
+}
+
+
+
